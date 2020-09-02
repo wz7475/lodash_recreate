b6c17089c990c5f08a91063ef17097e568cfe66a
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-09-02 16:59:21 +0200

    2

--- _.js
+++ _.js
@@ -1,4 +1,18 @@
 
+/* https://www.codecademy.com/paths/web-development/tracks/web-dev-js-arrays-loops-objects/modules/pjs-javascript-capstone/projects/lodash */
+
+const _ = 
+{
+    clamp(num, low, up) 
+    {
+        if (num >= low && num <= up)
+            return num;
+        else if (num < low)
+            return low;
+        else
+            return up;
+    }
+};
 
 
 
