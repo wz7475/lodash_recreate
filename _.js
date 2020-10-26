ab3b66809705b0b15f05fd5eda85e0fd10a115d0
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-10-26 21:01:12 +0100

    update

--- _.js
+++ _.js
@@ -1,16 +1,30 @@
 
 /* https://www.codecademy.com/paths/web-development/tracks/web-dev-js-arrays-loops-objects/modules/pjs-javascript-capstone/projects/lodash */
 
-const _ = 
+const _ =
 {
-    clamp(num, low, up) 
-    {
+    clamp(num, low, up) {
         if (num >= low && num <= up)
             return num;
         else if (num < low)
             return low;
         else
             return up;
+    },
+    inRange(num, start, end) {
+        if (start > end)
+        {
+            let buf = end;
+            end = start;
+            start = buf;
+        }
+        if (end === undefined) {
+            end = start;
+            start = 0;
+        }
+        if (num >= start && num < end)
+            return true;
+        return false;
     }
 };
 
